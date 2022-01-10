0566ec0ccf6b322ed36e1db379d4d30fd9b16c14
fernando <acostafernandoger@gmail.com>
2022-01-10 05:04:03 -0300

    API clima

---
+++ clima.js
@@ -0,0 +1,25 @@
+// clave: 0ed7ad2b11d4d18324891a793e66d94
+
+// https://api.openweathermap.org/data/2.5/weather?q=Villa%20Carlos%20Paz&appid=0ed7ad2b11d4d18324891a793e66d941&units=metric&lang=es
+
+async function fetchClima () {
+    let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Villa%20Carlos%20Paz&appid=0ed7ad2b11d4d18324891a793e66d941&units=metric&lang=es');
+
+    if (response.ok === false) {
+        throw new Error(response.status);
+    }
+
+    let clima = await response.json();
+
+    return clima;
+}
+
+fetchClima().then(clima => {
+    //Recien ahora tengo los datos
+    console.log(clima);
+
+//Mosrar el nombre de la ciudad en h2
+ciudad.innerHTML = clima.name;
+
+temperatura.innerHTML = "La temperatura es " + Math.round (clima.main.temp) + " °C";
+});
